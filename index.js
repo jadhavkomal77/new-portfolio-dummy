588a5950de61da63b0eb160a367d3089f62b9c3a
Komal Jadhav <komalmjadhav25@gmail.com>
2024-09-27 12:36:40 +0530

    dist added

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 const { adminProtected } = require("./middleware/Protected")
 require("dotenv").config({ path: "./.env" })   // env File Path 
 
 const app = express()
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true}))
 app.use(cookieParser())
+app.use(express.static("dist"))
 app.use(express.json())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
 app.use("/api/auth", require("./routes/auth.routes"))
-app.use("/api/admin",adminProtected, require("./routes/admin.routes"))
-app.use("/api/public",require("./routes/public.routes"))
+app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
+app.use("/api/public", require("./routes/public.routes"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resours Not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "Resours Not found" })
 })
 app.use((err, req, res, next) => {
     console.log(err)
