da87b21a6d9781209003f932daa147cd7a9ce516
Komal Jadhav <komalmjadhav25@gmail.com>
2024-10-04 14:07:16 +0530

    limit added

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,13 +1,14 @@
 const router = require("express").Router()
 const publicController = require("../controller/public.controller")
+const { rateLimit } = require("express-rate-limit")
 
 router
-    .get("/fetch-project", publicController.fetchProjects)
     .get("/fetch-caro", publicController.getAllCarousel)
+    .get("/fetch-project", publicController.fetchProjects)
     .get("/project-details/:id", publicController.getProjectDetail)
 
     .get("/fetch-enquery", publicController.fetchEnqueryMessage)
-    .post("/add-enquery", publicController.AddEnqueryMessage)
+    .post("/add-enquery", rateLimit({ windowMs: 1 * 60 * 1000, limit: 1 }), publicController.AddEnqueryMessage)
     .put("/update-enquery/:id", publicController.updateEnqueryMessage)
     .delete("/delete-enquery/:id", publicController.deleteEnqueryMessage)
 
